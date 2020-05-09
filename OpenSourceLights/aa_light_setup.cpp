4522122f4d1a35e9d13dba620cbde1f032d2df10
Anthony Barney <antbarney@outlook.com>
2020-05-08 22:49:55 -0700

    foo

--- OpenSourceLights/aa_light_setup.cpp
+++ OpenSourceLights/aa_light_setup.cpp
@@ -5,21 +5,20 @@
 
 // For each of the 8 states the light can have the following settings: On, Off, NA, Blink, FastBlink, or Dim. On/Off are defined below
 // These give us numerical values to these names which makes coding easier, we can just type in the name instead of the number.
-const uint16_t NA        = -1;
-const uint16_t BLINK     = -2;
-const uint16_t FASTBLINK = -3;
-const uint16_t SOFTBLINK = -4;
-const uint16_t DIM       = -5;
-const uint16_t FADEOFF   = -6;
-const uint16_t XENON     = -7;
-const uint16_t BACKFIRE  = -8;
+const byte NA        = -1;
+const byte BLINK     = -2;
+const byte FASTBLINK = -3;
+const byte SOFTBLINK = -4;
+const byte DIM       = -5;
+const byte FADEOFF   = -6;
+const byte XENON     = -7;
+const byte BACKFIRE  = -8;
+const byte ON        = 1;
+const byte OFF       = 0;
+const byte YES       = 1;
+const byte NO        = 0;
 
-const byte ON             = 1;
-const byte OFF            = 0;
-const byte YES            = 1;
-const byte NO             = 0;
-
-const uint16_t Schemes[NumSchemes][NumLights][NumStates] PROGMEM =
+const byte Schemes[NumSchemes][NumLights][NumStates] PROGMEM =
 {
   {
   //    IF CHANNEL 3 is only 3-position switch, values in Pos2 and Pos4 will be ignored (just use Pos1, Pos3, Pos5)
