f0a9312fc20c2d68e426209f2258afb1887a1396
Anthony Barney <antbarney@outlook.com>
2020-05-29 10:38:22 -0700

    foo

--- OpenSourceLights/simple_timer.h
+++ OpenSourceLights/simple_timer.h
@@ -6,12 +6,12 @@
   extern OSL_SimpleTimer timer;
   extern bool TimeUp;
 
+  void SetTimeUp();
   void BlinkLights();
+  void StopBlinking(unsigned int TimerID);
   void FastBlinkLights();
   unsigned int StartBlinking_ms(int LED, int BlinkTimes, int ms);
-  void StopBlinking(unsigned int TimerID);
   unsigned int StartWaiting_mS(int mS);
   unsigned int StartWaiting_sec(int seconds);
-  void SetTimeUp();
 
 #endif // ndef SIMPLE_TIMER_H
